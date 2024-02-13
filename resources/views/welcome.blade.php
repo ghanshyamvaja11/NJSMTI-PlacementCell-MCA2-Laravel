47cbfa79a9e017a48bb3bade2c591b9acae721eb
Ghanshyam Vaja <106793421+ghanshyamvaja11@users.noreply.github.com>
2024-02-13 18:57:06 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -191,8 +191,6 @@ function toggleMenu() {
 
 </main>
 @include('Layout.footer')
-</body>
-</html>
 
 <script>
 function Speech(){
@@ -231,3 +229,5 @@ function Speech(){
 }
 Speech();
     </script>
+</body>
+</html>
