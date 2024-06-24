4f39bb40545d15d8491e7e46fa47261842ac4e61
Ghanshyam Vaja <ghanshyamvaja11@gmail.com>
2024-06-24 07:02:56 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NJSMTI Placement Cell</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('Images/Header&Footer/favicon.png')}}">
+<link rel="icon" type="image/x-icon" href="{{asset('Images/Header&Footer/favicon.jpg')}}">
     <link rel="stylesheet" href="{{asset('css/headerNmenuNfooter.css')}}">
     <link rel="stylesheet" href="{{asset('css/index.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
